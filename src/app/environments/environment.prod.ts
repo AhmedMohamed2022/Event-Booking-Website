1c05215086daaa844f8132ec9e4142e03fa6bd6b
AhmedMohamed2022 <ahmedmohameddcb@gmail.com>
2025-08-22 20:45:21 +0300

    Fix: Remove double slash from apiUrl in production environment2

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -2,7 +2,7 @@
 export const environment = {
   production: true,
   apiUrl:
-    'https://37484aa9-9060-4a7d-985b-fdc8099f1d8c-00-3joc8gi3d72c5.spock.replit.dev',
+    'https://37484aa9-9060-4a7d-985b-fdc8099f1d8c-00-3joc8gi3d72c5.spock.replit.dev/api',
   // apiUrl: 'https://event-booking-backend-production-0d69.up.railway.app/api',
   appName: 'Event Booking Platform',
   version: '1.0.0',
